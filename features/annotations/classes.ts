0d04314f4ec47c04a9753ba315277228c8a5cdac
Samikshya <samikshyaaryal5@gmail.com>
2020-12-08 14:18:32 +0200

    created a project and defined random user

--- features/annotations/classes.ts
+++ features/annotations/classes.ts
@@ -1,5 +1,35 @@
 class Vehicle {
-  drive (): void{
-    console.log('kjsdc');
+  constructor( public color: string){}
+
+  protected honk(): void {
+    console.log('beep beep');
   }
+
+};
+
+
+//before passing any arguments 'orange' we need to define constructor function
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
+// accessing all the properties of class Vehicle by
+//child class
+//super in the class is a reference of constructor of the parent class
+class Car extends Vehicle {
+  constructor(public wheels:number, color: string){
+    super(color);
+  }
+  private drive(): void{
+    console.log('vroom');
+  } 
+
+  startDrivingProcess(): void {
+    this.drive();
+    this.honk();
+  }
+  
 }
+
+//instance of a car
+const car = new Car(4,'red');
+car.startDrivingProcess();
