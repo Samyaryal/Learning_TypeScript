bdfa8f60749122f3a25fb5b764792067b4079cd8
Samikshya <samikshyaaryal5@gmail.com>
2021-01-12 21:33:44 +0200

    adding marker property

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,17 +1,22 @@
+import { Mappable } from './CustomMap';
 import faker from 'faker';
 
-export class User{
+export class User implements Mappable { //instance of class user we create satisfies all the properties required by the Mappable interface
   name: string;
   location: {
     lat: number;
     lng: number;
   };
+  color: string = 'red';
 
   constructor(){
     this.name= faker.name.firstName();
     this.location ={
       lat:parseFloat(faker.address.latitude()),
       lng:parseFloat(faker.address.longitude())
-    }
+    };
+  }
+  markerContent (): string{
+    return `User Name: ${this.name}`;
   }
 }
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,4 +1,4 @@
- import {User} from './User';
+import {User} from './User';
 import {Company} from './Company';
 
 import {CustomMap} from './CustomMap';
